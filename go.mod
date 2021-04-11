b00bb2a91672e6720213795707db25c01b9b011a
Sp2-Hybrid <1171891648@qq.com>
2021-04-11 13:22:43 +0800

    graceful shutdown and reboot

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	github.com/astaxie/beego v1.12.3
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.7.1
 	github.com/go-ini/ini v1.62.0
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,57 @@
+
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-gin-example/pkg/setting"
 	"go-gin-example/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	router := routers.InitRouter()
 
 	s := &http.Server{
-		WriteTimeout:setting.WriteTimeout,
-		Addr:fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:router,
-		ReadTimeout:setting.ReadTimeout,
-		MaxHeaderBytes:1<<20,
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		// 开启一个goroutine启动服务
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
 		}
-	s.ListenAndServe()
+	}()
+
+	// 创建一个接收信号的通道
+	quit := make(chan os.Signal)
+	// kill和kill -1会默认发送syscall.SIGTERM信号
+	// kill -2 会发送syscall.SIGINT 信号
+	// kill -9 发送syscall.SIGKILL 信号，我们常见的Ctrl+C就是触发这个信号
+	// signal.Notify 将收到的信号转发给quit，这一步并不会阻塞
+	signal.Notify(quit, os.Interrupt)
+
+	// 在这一步会阻塞，当接收到信号才会往下执行
+	<- quit
+
+	log.Println("Shutdown Server ...")
+	// 创建一个超时5秒的context
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	// 5秒内关闭服务（将未处理完的请求处理完再关闭服务），超时5秒就自动退出
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
